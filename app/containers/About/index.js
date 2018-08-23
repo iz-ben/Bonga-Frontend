3dcbde5f37a522cf534cabf7bd404591a586f67d
Ben <ben@coterie.co.ke>
2018-08-23 18:22:49 +0300

    About container

---
+++ app/containers/About/index.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * About
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectAbout from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+/* eslint-disable react/prefer-stateless-function */
+export class About extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+About.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  about: makeSelectAbout(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key: 'about', reducer });
+const withSaga = injectSaga({ key: 'about', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(About);
