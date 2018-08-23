ba4c703c76ff7ffe94090c4e1f814f70e1de662b
Ben <ben@coterie.co.ke>
2018-08-23 18:50:48 +0300

    add containers

---
+++ app/components/Footer/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Footer
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class Footer extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+Footer.propTypes = {};
+
+export default Footer;
---
+++ app/components/Loading/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Loading
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class Loading extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+Loading.propTypes = {};
+
+export default Loading;
