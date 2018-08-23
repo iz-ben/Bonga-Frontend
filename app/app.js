abee32b55b79aba2ee8faaec97fa685d9b0aff66
Ben <ben@coterie.co.ke>
2018-08-23 19:08:52 +0300

    atttach material

--- app/app.js
+++ app/app.js
@@ -26,15 +26,15 @@ import LanguageProvider from 'containers/LanguageProvider';
 /* eslint-disable import/no-unresolved, import/extensions */
 import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import 'file-loader?name=[name].[ext]!./.htaccess';
+// Import CSS reset and Global Styles
+// import './global-styles';
+import 'assets/scss/material-kit-react.css?v=1.1.0';
 /* eslint-enable import/no-unresolved, import/extensions */
 
 import configureStore from './configureStore';
 
 // Import i18n messages
 import { translationMessages } from './i18n';
-
-// Import CSS reset and Global Styles
-import './global-styles';
 
 // Create redux store with history
 const initialState = {};
