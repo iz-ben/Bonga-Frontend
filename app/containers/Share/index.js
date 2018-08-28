7cccce01655183558e01409aa422feb4b3f2df83
Ben <ben@coterie.co.ke>
2018-08-28 16:24:10 +0300

    keep the loader from hiding page when there's content already loaded

--- app/containers/Share/index.js
+++ app/containers/Share/index.js
@@ -14,8 +14,8 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ReactPaginate from 'react-paginate';
 
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 
 import GridContainer from 'components/Grid/GridContainer';
 import GridItem from 'components/Grid/GridItem';
@@ -24,40 +24,37 @@ import pageStyle from 'assets/jss/views/pageStyle';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
+import Loading from 'components/Loading';
+import Editor from 'components/Editor/Loadable';
+import { trackView } from 'utils/analyticsUtil';
 import makeSelectShare, {
   makeSelectCurrentPage,
   makeSelectEditorOpen,
   makeSelectNetworkActive,
   makeSelectPages,
-  makeSelectStories, makeSelectVisibleItems,
+  makeSelectVisibleItems,
 } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import { getComments } from './actions';
-import Loading from 'components/Loading';
-import Editor from 'components/Editor/Loadable';
-import { trackView } from 'utils/analyticsUtil';
 
 /* eslint-disable react/prefer-stateless-function */
 class Share extends React.Component {
-
-  handlePaginationChange = ({selected})=>{
-    //console.log(selected);
-    const page = ++selected;
+  handlePaginationChange = ({ selected }) => {
+    // console.log(selected);
+    const page = selected + 1;
     this.props.dispatch(getComments(page));
   };
 
-  componentDidMount()
-  {
+  componentDidMount() {
     const currentPage = this.props.current || 0;
-    this.props.dispatch(getComments( currentPage ));
+    this.props.dispatch(getComments(currentPage));
     trackView(this.props.location);
   }
   render() {
-    const { classes, stories, current, pages, editorOpen, ...rest } = this.props;
-    if(this.props.loading)
-    {
-      return <Loading/>;
+    const { classes, stories, current } = this.props;
+    if (this.props.loading && !stories.length) {
+      return <Loading />;
     }
     return (
       <div>
@@ -67,37 +64,39 @@ class Share extends React.Component {
         </Helmet>
         <div className={classes.container}>
           <GridContainer>
-            <GridItem  xs={12} sm={12} md={12}>
-              <Editor/>
-              <div className={`story-wrapper`}>
+            <GridItem xs={12} sm={12} md={12}>
+              <Editor />
+              <div className="story-wrapper">
                 <TransitionGroup className="todo-list">
-                  {stories.map(story=>(
+                  {stories.map(story => (
                     <CSSTransition
                       key={story.id}
                       timeout={500}
                       classNames="fade"
-                    ><StoryItem {...story} /></CSSTransition>
+                    >
+                      <StoryItem {...story} />
+                    </CSSTransition>
                   ))}
-
                 </TransitionGroup>
-
               </div>
             </GridItem>
           </GridContainer>
           <div className="pagination" id="react-paginate">
-            {this.props.pages > 1 ? <ReactPaginate previousLabel={<ChevronLeft/>}
-                                                   nextLabel={<ChevronRight/>}
-                                                   breakLabel={<a href="">...</a>}
-                                                   breakClassName={"break-me"}
-                                                   pageCount={this.props.pages}
-                                                   marginPagesDisplayed={2}
-                                                   pageRangeDisplayed={5}
-                                                   onPageChange={this.handlePaginationChange}
-                                                   subContainerClassName={"pages pagination"}
-                                                   forcePage = {current}
-                                                   activeClassName={"active"} />
-              :''
-            }
+            {this.props.pages > 1 ? (
+              <ReactPaginate
+                previousLabel={<ChevronLeft />}
+                nextLabel={<ChevronRight />}
+                breakLabel={<a href={this.props.location.pathname}>...</a>}
+                breakClassName="break-me"
+                pageCount={this.props.pages}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={this.handlePaginationChange}
+                subContainerClassName="pages pagination"
+                forcePage={current}
+                activeClassName="active"
+              />
+            ) : null}
           </div>
         </div>
       </div>
@@ -107,21 +106,22 @@ class Share extends React.Component {
 
 Share.propTypes = {
   dispatch: PropTypes.func.isRequired,
-  stories:PropTypes.array,
-  editorOpen:PropTypes.bool,
-  loading:PropTypes.bool,
-  pages:PropTypes.number,
-  current:PropTypes.number
-
+  stories: PropTypes.array,
+  editorOpen: PropTypes.bool,
+  loading: PropTypes.bool,
+  pages: PropTypes.number,
+  current: PropTypes.number,
+  location: PropTypes.object,
+  classes: PropTypes.object,
 };
 
 const mapStateToProps = createStructuredSelector({
   share: makeSelectShare(),
-  stories:makeSelectVisibleItems(),
-  editorOpen:makeSelectEditorOpen(),
-  loading:makeSelectNetworkActive(),
-  pages:makeSelectPages(),
-  current:makeSelectCurrentPage()
+  stories: makeSelectVisibleItems(),
+  editorOpen: makeSelectEditorOpen(),
+  loading: makeSelectNetworkActive(),
+  pages: makeSelectPages(),
+  current: makeSelectCurrentPage(),
 });
 
 function mapDispatchToProps(dispatch) {
